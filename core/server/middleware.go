227359d3dab5cc290f84f755992f8fe84499ab7f
Baloo <73783549+41Baloo@users.noreply.github.com>
2023-03-06 10:12:56 +0000

    Fixed Accidentally Minified Captcha

--- core/server/middleware.go
+++ core/server/middleware.go
@@ -245,7 +245,231 @@ func Middleware(writer http.ResponseWriter, request *http.Request) {
 			}
 
 			writer.Header().Set("Content-Type", "text/html")
-			fmt.Fprintf(writer, `<html><head><style>body{background-color:#f5f5f5;font-family:Arial,sans-serif}.center{display:flex;align-items:center;justify-content:center;height:100vh}.box{background-color:#fff;border:1px solid #ddd;border-radius:4px;padding:20px;width:500px}canvas{display:block;margin:0 auto;max-width:100%%;width:100%%;height:auto}input[type=text]{width:100%%;padding:12px 20px;margin:8px 0;box-sizing:border-box;border:2px solid #ccc;border-radius:4px}button{width:100%%;background-color:#4caf50;color:#fff;padding:14px 20px;margin:8px 0;border:none;border-radius:4px;cursor:pointer}button:hover{background-color:#45a049}.box{background-color:#fff;border:1px solid #ddd;border-radius:4px;padding:20px;width:500px;transition:height .1s;position:block}.box *{transition:opacity .1s}.success{background-color:#dff0d8;border:1px solid #d6e9c6;border-radius:4px;color:#3c763d;padding:20px}.failure{background-color:#f0d8d8;border:1px solid #e9c6c6;border-radius:4px;color:#763c3c;padding:20px}.collapsible{background-color:#f5f5f5;color:#444;cursor:pointer;padding:18px;width:100%%;border:none;text-align:left;outline:0;font-size:15px}.collapsible:after{content:'\002B';color:#777;font-weight:700;float:right;margin-left:5px}.collapsible.active:after{content:"\2212"}.collapsible:hover{background-color:#e5e5e5}.collapsible-content{padding:0 18px;max-height:0;overflow:hidden;transition:max-height .2s ease-out;background-color:#f5f5f5}</style></head><body><div class="center" id="center"><div class="box" id="box"><h1>Enter the<b>green</b>text you see in the picture</h1><canvas id="image" width="100" height="37"></canvas><form onsubmit="return checkAnswer(event)"><input id="text" type="text" maxlength="6" placeholder="Solution" required><button type="submit">Submit</button></form><div class="success" id="successMessage" style="display:none">Success! Redirecting ...</div><div class="failure" id="failMessage" style="display:none">Failed! Please try again.</div><button class="collapsible">Why am I seeing this page?</button><div class="collapsible-content"><p>The website you are trying to visit needs to make sure that you are not a bot. This is a common security measure to protect websites from automated spam and abuse. By entering the characters you see in the picture, you are helping to verify that you are a real person.</p></div></div></div></body><script>let canvas=document.getElementById("image"),ctx=canvas.getContext("2d");var i,image=new Image;function checkAnswer(e){e.preventDefault();var t=document.getElementById("text").value;document.cookie="%s_3__bProxy_v="+t+"%s; SameSite=Lax; path=/; Secure",fetch("https://"+location.hostname+"/_bProxy/verified").then(function(e){return e.text()}).then(function(e){"verified"===e?(document.getElementById("successMessage").style.display="block",setInterval(function(){var e=document.getElementById("box"),t=e.offsetHeight,a=setInterval(function(){t-=20,e.style.height=t+"px";for(var l=e.children,n=0;n<l.length;n++)l[n].style.opacity=0;t<=0&&(e.style.height="0",e.remove(),clearInterval(a),location.reload())},20)},1e3)):(document.getElementById("failMessage").style.display="block",setInterval(function(){location.reload()},1e3))}).catch(function(e){document.getElementById("failMessage").style.display="block",setInterval(function(){location.reload()},1e3)})}image.onload=function(){ctx.drawImage(image,(canvas.width-image.width)/2,(canvas.height-image.height)/2)},image.src="data:image/png;base64,%s";var coll=document.getElementsByClassName("collapsible");for(i=0;i<coll.length;i++)coll[i].addEventListener("click",function(){this.classList.toggle("active");var e=this.nextElementSibling;e.style.maxHeight?e.style.maxHeight=null:e.style.maxHeight=e.scrollHeight+"px"});</script>`, captchaData, ip, publicPart)
+			fmt.Fprintf(writer,
+				`
+					<html>
+					<head>
+						<style>
+						body {
+							background-color: #f5f5f5;
+							font-family: Arial, sans-serif;
+						}
+
+						.center {
+							display: flex;
+							align-items: center;
+							justify-content: center;
+							height: 100vh;
+						}
+
+						.box {
+							background-color: white;
+							border: 1px solid #ddd;
+							border-radius: 4px;
+							padding: 20px;
+							width: 500px;
+						}
+
+						canvas {
+							display: block;
+							margin: 0 auto;
+							max-width: 100%%;
+							width: 100%%;
+    						height: auto;
+						}
+
+						input[type="text"] {
+							width: 100%%;
+							padding: 12px 20px;
+							margin: 8px 0;
+							box-sizing: border-box;
+							border: 2px solid #ccc;
+							border-radius: 4px;
+						}
+
+						button {
+							width: 100%%;
+							background-color: #4caf50;
+							color: white;
+							padding: 14px 20px;
+							margin: 8px 0;
+							border: none;
+							border-radius: 4px;
+							cursor: pointer;
+						}
+
+						button:hover {
+							background-color: #45a049;
+						}
+						/* Add styles for the animation */ 
+
+	.box {
+		background-color: white;
+		border: 1px solid #ddd;
+		border-radius: 4px;
+		padding: 20px;
+		width: 500px;
+		/* Add a transition effect for the height */ 
+		transition: height 0.1s;
+		position: block;
+	}
+	/* Add a transition effect for the opacity */ 
+
+	.box * {
+		transition: opacity 0.1s;
+	}
+	/* Add a success message and style it */ 
+
+	.success {
+		background-color: #dff0d8;
+		border: 1px solid #d6e9c6;
+		border-radius: 4px;
+		color: #3c763d;
+		padding: 20px;
+	}
+
+	.failure {
+		background-color: #f0d8d8;
+		border: 1px solid #e9c6c6;
+		border-radius: 4px;
+		color: #763c3c;
+		padding: 20px;
+	}
+	/* Add styles for the collapsible help text */ 
+
+						.collapsible {
+							background-color: #f5f5f5;
+							color: #444;
+							cursor: pointer;
+							padding: 18px;
+							width: 100%%;
+							border: none;
+							text-align: left;
+							outline: none;
+							font-size: 15px;
+						}
+
+						.collapsible:after {
+							content: '\002B';
+							color: #777;
+							font-weight: bold;
+							float: right;
+							margin-left: 5px;
+						}
+
+						.collapsible.active:after {
+							content: "\2212";
+						}
+
+						.collapsible:hover {
+							background-color: #e5e5e5;
+						}
+
+						.collapsible-content {
+							padding: 0 18px;
+							max-height: 0;
+							overflow: hidden;
+							transition: max-height 0.2s ease-out;
+							background-color: #f5f5f5;
+						}
+						</style>
+					</head>
+					<body>
+						<div class="center" id="center">
+							<div class="box" id="box">
+								<h1>Enter the <b>green</b> text you see in the picture</h1>  <canvas id="image" width="100" height="37"></canvas>
+								<form onsubmit="return checkAnswer(event)">
+									<input id="text" type="text" maxlength="6" placeholder="Solution" required>
+									<button type="submit">Submit</button>
+								</form>
+								<div class="success" id="successMessage" style="display: none;">Success! Redirecting ...</div>
+								<div class="failure" id="failMessage" style="display: none;">Failed! Please try again.</div>
+								<button class="collapsible">Why am I seeing this page?</button>
+								<div class="collapsible-content">
+									<p> The website you are trying to visit needs to make sure that you are not a bot. This is a common security measure to protect websites from automated spam and abuse. By entering the characters you see in the picture, you are helping to verify that you are a real person. </p>
+								</div>
+							</div>
+						</div>
+					</body>
+					<script>
+					let canvas=document.getElementById("image");
+					let ctx = canvas.getContext("2d");
+					var image = new Image();
+					image.onload = function() {
+						ctx.drawImage(image, (canvas.width-image.width)/2, (canvas.height-image.height)/2);
+					};
+					image.src = "data:image/png;base64,%s";
+					function checkAnswer(event) {
+						// Prevent the form from being submitted
+						event.preventDefault();
+						// Get the user's input
+						var input = document.getElementById('text').value;
+
+						document.cookie = '%s_3__bProxy_v='+input+'%s; SameSite=Lax; path=/; Secure';
+
+						// Check if the input is correct
+						fetch('https://' + location.hostname + '/_bProxy/verified').then(function(response) {
+							return response.text();
+						}).then(function(text) {
+							if(text === 'verified') {
+								// If the answer is correct, show the success message
+								var successMessage = document.getElementById("successMessage");
+								successMessage.style.display = "block";
+
+								setInterval(function(){
+									// Animate the collapse of the box
+									var box = document.getElementById("box");
+									var height = box.offsetHeight;
+									var collapse = setInterval(function() {
+										height -= 20;
+										box.style.height = height + "px";
+										// Reduce the opacity of the child elements as the box collapses
+										var elements = box.children;
+										//successMessage.remove()
+										for(var i = 0; i < elements.length; i++) {
+											elements[i].style.opacity = 0
+										}
+										if(height <= 0) {
+											// Set the height of the box to 0 after the collapse is complete
+											box.style.height = "0";
+											// Stop the collapse animation
+											box.remove()
+											clearInterval(collapse);
+											location.reload();
+										}
+									}, 20);
+								}, 1000)
+							} else {
+								var failMessage = document.getElementById('failMessage');
+								failMessage.style.display = 'block';
+								setInterval(function() {
+									location.reload()
+								}, 1000)
+							}
+						}).catch(function(err){
+							var failMessage = document.getElementById('failMessage');
+							failMessage.style.display = 'block';
+							setInterval(function() {
+								location.reload()
+							}, 1000)
+						})
+					}
+					// Add JavaScript to toggle the visibility of the collapsible content
+					var coll = document.getElementsByClassName("collapsible");
+					var i;
+					for(i = 0; i < coll.length; i++) {
+						coll[i].addEventListener("click", function() {
+							this.classList.toggle("active");
+							var content = this.nextElementSibling;
+							if(content.style.maxHeight) {
+								content.style.maxHeight = null;
+							} else {
+								content.style.maxHeight = content.scrollHeight + "px";
+							}
+						});
+					}
+					</script>
+					`, captchaData, ip, publicPart)
 			return
 		default:
 			writer.Header().Set("Content-Type", "text/plain")
